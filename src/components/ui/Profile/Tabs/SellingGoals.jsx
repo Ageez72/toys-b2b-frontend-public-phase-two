eb08270781cd5c1daeb18949684a1a39bbb43bc6
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2025-09-27 19:00:19 +0300

    new updates

--- src/components/ui/Profile/Tabs/SellingGoals.jsx
+++ src/components/ui/Profile/Tabs/SellingGoals.jsx
@@ -5,15 +5,22 @@ import en from "../../../../../locales/en.json";
 import ar from "../../../../../locales/ar.json";
 import TabPanel from '../TabPanel';
 import SellingTable from './SellingTable';
+import { BASE_API, endpoints } from '../../../../../constant/endpoints';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import Loader from '../../Loaders/Loader';
 
 export default function SellingGoals({ order }) {
     const { state = {}, dispatch = () => { } } = useAppContext() || {};
     const [translation, setTranslation] = useState(ar);
     const [activeTab, setActiveTab] = useState('selling-brand');
+    const [targetData, setTargetData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setTranslation(state.LANG === "EN" ? en : ar);
         document.title = state.LANG === 'AR' ? ar.statementOfAccount : en.statementOfAccount;
+        getTargetData();
     }, [state.LANG]);
 
     const currentDate = new Date();
@@ -28,45 +35,97 @@ export default function SellingGoals({ order }) {
 
     const handleTabClick = (tabId) => {
         setActiveTab(tabId);
-        router.replace(`/profile?${tabId}`);
+    };
+
+    const getTargetData = async () => {
+        setIsLoading(true);
+        try {
+            const res = await axios.get(
+                `${BASE_API}${endpoints.products.getTarget}&lang=${state.LANG}&token=${Cookies.get('token')}`
+            );
+
+            // Transform API response to a unified format
+            const formattedData = [];
+
+            res.data.forEach((obj) => {
+                const key = Object.keys(obj)[0]; // e.g. "brand", "group brand"
+                formattedData.push({
+                    type: key.includes("brand") && !key.includes("group") ? "brand"
+                        : key.includes("group brand") ? "group"
+                            : key.includes("product") ? "product"
+                                : key.includes("classification") ? "classification"
+                                    : "other",
+                    items: obj[key]
+                });
+            });
+            setTargetData(formattedData);
+            setIsLoading(false);
+        } catch (err) {
+            console.error("Error fetching target data:", err);
+            setIsLoading(false);
+        }
     };
 
     return (
-        <div className="py-3">
-            <h2 className='sub-title mb-6'>{translation.sellingGoals} {currentYear}</h2>
-            <ul className={`pt-3 profile-selling-tabs-links ${state.isCorporate && state.isActive ? 'is-corporate-account' : ''}`}>
-                {profileTabs.map((tab) => (
-                    <li key={tab.id} className={tab.id}>
-                        <button
-                            onClick={() => handleTabClick(tab.id)}
-                            className={`flex items-center justify-between w-full text-right cursor-pointer px-2 py-2 rounded-md ${activeTab === tab.id ? 'active' : 'hover:bg-gray-100'}`}
-                        >
-                            <span className="flex items-center">
-                                <span className="flex items-center justify-between block px-2 py-2">
-                                    {tab.label}
-                                </span>
-                            </span>
-                        </button>
-                    </li>
-                ))}
-            </ul>
+        <>
+            {isLoading ? <Loader /> : null}
+            {
+                targetData.length === 0 ? (
+                    <div className='empty-state text-center my-20'>
+                        <svg className='mx-auto' xmlns="http://www.w3.org/2000/svg" width="260" height="260" viewBox="0 0 260 260" fill="none">
+                            <rect width="260" height="260" rx="130" fill="url(#paint0_linear_1451_4175)" />
+                            <path opacity="0.4" d="M152.696 75.8333H107.305C87.5882 75.8333 75.834 87.5875 75.834 107.304V152.642C75.834 172.413 87.5882 184.167 107.305 184.167H152.642C172.359 184.167 184.113 172.413 184.113 152.696V107.304C184.167 87.5875 172.413 75.8333 152.696 75.8333Z" fill="#7E818E" />
+                            <path d="M153.941 107.304V152.696C153.941 156.163 151.124 158.979 147.658 158.979C144.137 158.979 141.32 156.163 141.32 152.696V107.304C141.32 103.838 144.137 101.021 147.658 101.021C151.124 101.021 153.941 103.838 153.941 107.304Z" fill="#7E818E" />
+                            <path d="M118.679 135.038V152.696C118.679 156.163 115.863 158.979 112.342 158.979C108.875 158.979 106.059 156.163 106.059 152.696V135.038C106.059 131.571 108.875 128.754 112.342 128.754C115.863 128.754 118.679 131.571 118.679 135.038Z" fill="#7E818E" />
+                            <defs>
+                                <linearGradient id="paint0_linear_1451_4175" x1="130" y1="0" x2="130" y2="260" gradientUnits="userSpaceOnUse">
+                                    <stop stopColor="#E9EBED" />
+                                    <stop offset="1" stopColor="white" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                        <h2 className='sub-title'>{translation.products.resultsNotFound}</h2>
+                    </div>
+                ) : (
 
-            <div className="responsive-table">
-                <div className="selling-target-table">
-                    <TabPanel id="selling-brand" activeTab={activeTab}>
-                        <SellingTable />
-                    </TabPanel>
-                    <TabPanel id="selling-group" activeTab={activeTab}>
-                        test 2
-                    </TabPanel>
-                    <TabPanel id="selling-product" activeTab={activeTab}>
-                        test 3
-                    </TabPanel>
-                    <TabPanel id="selling-classification" activeTab={activeTab}>
-                        test 4
-                    </TabPanel>
-                </div>
-            </div>
-        </div>
+                    <div className="py-3">
+                        <h2 className='sub-title mb-6'>{translation.sellingGoals} {currentYear}</h2>
+                        <ul className={`pt-3 profile-selling-tabs-links ${state.isCorporate && state.isActive ? 'is-corporate-account' : ''}`}>
+                            {profileTabs.map((tab) => (
+                                <li key={tab.id} className={tab.id}>
+                                    <button
+                                        onClick={() => handleTabClick(tab.id)}
+                                        className={`flex items-center justify-between w-full text-right cursor-pointer px-2 py-2 rounded-md ${activeTab === tab.id ? 'active' : 'hover:bg-gray-100'}`}
+                                    >
+                                        <span className="flex items-center">
+                                            <span className="flex items-center justify-between block px-2 py-2">
+                                                {tab.label}
+                                            </span>
+                                        </span>
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className="responsive-table">
+                            <div className="selling-target-table">
+                                <TabPanel id="selling-brand" activeTab={activeTab}>
+                                    <SellingTable data={targetData.find(item => item.type === 'brand')?.items || []} />
+                                </TabPanel>
+                                <TabPanel id="selling-group" activeTab={activeTab}>
+                                    <SellingTable data={targetData.find(item => item.type === 'group')?.items || []} />
+                                </TabPanel>
+                                <TabPanel id="selling-product" activeTab={activeTab}>
+                                    <SellingTable data={targetData.find(item => item.type === 'product')?.items || []} />
+                                </TabPanel>
+                                <TabPanel id="selling-classification" activeTab={activeTab}>
+                                    <SellingTable data={targetData.find(item => item.type === 'classification')?.items || []} />
+                                </TabPanel>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
--- src/components/ui/Profile/Tabs/SellingTable.jsx
+++ src/components/ui/Profile/Tabs/SellingTable.jsx
@@ -1,17 +1,12 @@
 "use client"
-import Reac, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAppContext } from '../../../../../context/AppContext';
 import en from "../../../../../locales/en.json";
 import ar from "../../../../../locales/ar.json";
 
-export default function SellingTable() {
-    const { state = {}, dispatch = () => { } } = useAppContext() || {};
-    const [translation, setTranslation] = useState(ar);
-
-
-    useEffect(() => {
-        setTranslation(state.LANG === "EN" ? en : ar);
-    }, [state.LANG]);
+export default function SellingTable({ data }) {
+    const { state = {} } = useAppContext() || {};
+    const translation = state.LANG === "EN" ? en : ar;
 
     return (
         <div className="selling-table-container">
@@ -20,52 +15,61 @@ export default function SellingTable() {
                 <div className='w-1/4'>{translation.address}</div>
                 <div className='w-1/2'>{translation.target}</div>
             </div>
-            <div className="table-item flex mb-12">
-                <div className='w-1/4 px-3'>1</div>
-                <div className='w-1/4 px-3'>هنا يضاف اسم المنتج</div>
-                <div className='w-1/2 px-3 target-content'>
-                    <div className='flex justify-between mb-2'>
-                        <p className='mb-0 font-bold'>{translation.wantTarget}</p>
-                        <p className='mb-0 font-bold'>
-                            <span>10,000</span>
-                            <span className="price-unit mx-1">{translation.jod}</span>
-                        </p>
+
+            {data.length === 0 && (
+                <>
+                    <div className='empty-state text-center my-20'>
+                        <svg className='mx-auto' xmlns="http://www.w3.org/2000/svg" width="260" height="260" viewBox="0 0 260 260" fill="none">
+                            <rect width="260" height="260" rx="130" fill="url(#paint0_linear_422_4029)" />
+                            <path opacity="0.4" d="M184 96.2665C184 99.8665 182.067 103.133 179 104.8L167.4 111.066L157.533 116.333L137.067 127.4C134.867 128.6 132.467 129.2 130 129.2C127.533 129.2 125.133 128.6 122.933 127.4L81 104.8C77.9333 103.133 76 99.8665 76 96.2665C76 92.6665 77.9333 89.3996 81 87.7329L94.1333 80.6663L104.6 74.9998L122.933 65.1333C127.333 62.7333 132.667 62.7333 137.067 65.1333L179 87.7329C182.067 89.3996 184 92.6665 184 96.2665Z" fill="#7E818E" />
+                            <path opacity="0.4" d="M116 135.267L76.9999 115.733C73.9999 114.2 70.5333 114.4 67.6666 116.133C64.7999 117.867 63.1333 120.934 63.1333 124.267V161.133C63.1333 167.533 66.6665 173.267 72.3999 176.133L111.4 195.6C112.733 196.267 114.2 196.6 115.667 196.6C117.4 196.6 119.133 196.134 120.667 195.134C123.533 193.4 125.2 190.333 125.2 187V150.134C125.267 143.867 121.733 138.133 116 135.267Z" fill="#7E818E" />
+                            <path opacity="0.4" d="M196.867 124.333V161.2C196.867 167.533 193.334 173.267 187.6 176.133L148.6 195.666C147.267 196.333 145.8 196.667 144.333 196.667C142.6 196.667 140.867 196.2 139.267 195.2C136.467 193.467 134.733 190.4 134.733 187.067V150.267C134.733 143.867 138.267 138.133 144 135.267L158.333 128.133L168.333 123.133L183 115.8C186 114.267 189.467 114.4 192.333 116.2C195.133 117.933 196.867 121 196.867 124.333Z" fill="#7E818E" />
+                            <path d="M167.4 111.066L157.533 116.333L94.1333 80.6665L104.6 75L165.8 109.533C166.467 109.933 167 110.466 167.4 111.066Z" fill="#7E818E" />
+                            <path d="M168.333 123.133V138.267C168.333 141 166.067 143.267 163.333 143.267C160.6 143.267 158.333 141 158.333 138.267V128.133L168.333 123.133Z" fill="#7E818E" />
+                            <defs>
+                                <linearGradient id="paint0_linear_422_4029" x1="130" y1="0" x2="130" y2="260" gradientUnits="userSpaceOnUse">
+                                    <stop stopColor="#E9EBED" />
+                                    <stop offset="1" stopColor="white" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                        <h2 className='sub-title mt-5'>{translation.noData}</h2>
                     </div>
-                    <div className="progress-bar">
-                        <div className="progress" style={{ width: `calc(${22.5}% - 8px)` }}></div>
+                </>
+            )}
+
+            {data.map((item, index) => {
+                const progressPerc = item.perc > 100 ? 100 : item.perc;
+                const remaining = item.target - item.achieved;
+
+                return (
+                    <div key={index} className="table-item flex mb-12">
+                        <div className='w-1/4 px-3'>{index + 1}</div>
+                        <div className='w-1/4 px-3'>{item.name}</div>
+                        <div className='w-1/2 px-3 target-content'>
+                            <div className='flex justify-between mb-2'>
+                                <p className='mb-0 font-bold'>{translation.wantTarget}</p>
+                                <p className='mb-0 font-bold'>
+                                    <span>{item.target}</span>
+                                    <span className="price-unit mx-1">{translation.jod}</span>
+                                </p>
+                            </div>
+                            <div className="progress-bar">
+                                <div className="progress" style={{ width: `calc(${progressPerc}% - 8px)` }}></div>
+                            </div>
+                            <div className='flex justify-between mt-2 red-text'>
+                                <p className='mb-0 font-bold'>
+                                    تم تحقيق {item.achieved} {translation.jod} من {item.target} {translation.jod}
+                                </p>
+                                <p className='mb-0 font-bold'>
+                                    <span>{translation.remaining} </span>
+                                    <span>{remaining > 0 ? remaining : 0}</span>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex justify-between mt-2 red-text'>
-                        <p className='mb-0 font-bold'>تم تحقيق 6,500 دأ من 10,000 د.أ</p>
-                        <p className='mb-0 font-bold'>
-                            <span>{translation.remaining} </span>
-                            <span>5,000</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="table-item flex mb-12">
-                <div className='w-1/4 px-3'>2</div>
-                <div className='w-1/4 px-3'>هنا يضاف اسم المنتج</div>
-                <div className='w-1/2 px-3 target-content'>
-                    <div className='flex justify-between mb-2'>
-                        <p className='mb-0 font-bold'>{translation.wantTarget}</p>
-                        <p className='mb-0 font-bold'>
-                            <span>10,000</span>
-                            <span className="price-unit mx-1">{translation.jod}</span>
-                        </p>
-                    </div>
-                    <div className="progress-bar">
-                        <div className="progress" style={{ width: `calc(${22.5}% - 8px)` }}></div>
-                    </div>
-                    <div className='flex justify-between mt-2 red-text'>
-                        <p className='mb-0 font-bold'>تم تحقيق 6,500 دأ من 10,000 د.أ</p>
-                        <p className='mb-0 font-bold'>
-                            <span>{translation.remaining} </span>
-                            <span>5,000</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
+                );
+            })}
         </div>
     )
 }
