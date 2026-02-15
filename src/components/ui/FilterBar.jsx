3a61329e90d69d5deb757119b3db4a3a9f3bfcea
mohamed.sobhy <mohamed.sobhy@linkdev.com>
2026-02-15 11:12:22 +0200

    test

--- src/components/ui/FilterBar.jsx
+++ src/components/ui/FilterBar.jsx
@@ -474,7 +474,7 @@ export default function FilterBar({ isProductsPage, resetUpperFilters, catalogEn
                                         // <Suspense fallback={<div>Loading...</div>}>
                                         //     <MultiRangeSliderAge initiallyOpen={true} title={translation.ageRange} min={Math.floor(parseFloat(filtersSections?.age_min))} max={Math.floor(parseFloat(filtersSections?.age_max))} selectedFrom={fromAge} selectedTo={toAge} handleAgeFrom={changeAgeFrom} handleAgeTo={changeAgeTo} isProductsPage={isProductsPage} onSubmitRange={handleApplyFilters} onClearRange={handleClearFilter} />
                                         // </Suspense>
-                                        <FilterSingleItem inputType="radio" title={translation.ageRange} selected={selectedAge} options={agesAllOptions} initiallyOpen={true} name="age" handleSingleItem={changeSingleItem} />
+                                        <FilterSingleItem inputType="checkbox" title={translation.ageRange} selected={selectedAge} options={agesAllOptions} initiallyOpen={true} name="age" handleSingleItem={changeSingleItem} />
                                     ) : null
                                 }
                                 <FilterSingleItem inputType="radio" initiallyOpen={true} title={translation.availablity} selected={itemStatus} options={StatusOptions} name="itemStatus" handleSingleItem={changeSingleItem} />
